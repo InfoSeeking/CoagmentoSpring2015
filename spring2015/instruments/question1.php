886b76aaa8cdd81921dfb70b2277ea8aa455049c
mmitsui <mmitsui@scarletmail.rutgers.edu>
2015-02-01 08:17:05 -0500

    Dynamic question generation

--- spring2015/instruments/question1.php
+++ spring2015/instruments/question1.php
@@ -5,26 +5,26 @@
     require_once('../core/Stage.class.php');
 	require_once('../core/Action.class.php');
 	require_once('../core/Util.class.php');
-	
+
 	Util::getInstance()->checkSession();
 	$base = new Base();
-	
-	
+
+
 	/*
-     
+
      ---Find the  the user's topic area---
-     
+
      SELECT u.userID, u.topicAreaID, u.sessionID
      FROM user_session_topic u
      WHERE u.userID='$userID' AND u.sessionID='$sessionID'
      --Then in where clause of retrieving the question form questions study have---
      WHERE topicAreaID checked
-     
+
      */
 	// echo "Hello World";
 	if (Util::getInstance()->checkCurrentPage(basename( __FILE__ )))
 	{
-        
+
 		//$base = new Base();
 		$projectID = $base->getProjectID();
 		$userID = $base->getUserID();
@@ -34,64 +34,64 @@
 		$stageID = $base->getStageID();
 		$questionID = $base->getQuestionID();
 		$collaborativeStudy = $base->getStudyID();
-        
-		
+
+
 		$sessionID=1;
-        
+
         $qQuery = "SELECT numUsers from users WHERE userID='$userID'";
         $connection = Connection::getInstance();
         $results = $connection->commit($qQuery);
         $line = mysql_fetch_array($results,MYSQL_ASSOC);
         $num_users = $line['numUsers'];
-        
-        
+
+
         $qQuery = "SELECT userID, topicAreaID, sessionID
         FROM user_session_topic
         WHERE userID='$userID' AND sessionID='$sessionID'";
-        
-        
-        
-        
+
+
+
+
         $connection = Connection::getInstance();
         $results = $connection->commit($qQuery);
         $numRows = mysql_num_rows($results);
-        
+
         $topicAreaID = -1;
         if($numRows>0)
         {
             $line = mysql_fetch_array($results, MYSQL_ASSOC);
             $topicAreaID = $line['topicAreaID'];
         }
-        
+
 		if (isset($_POST['pretask']))
 		{
-            
+
             $localDate = $_POST['localDate'];
             $localTime = $_POST['localTime'];
             $localTimestamp = $_POST['localTimestamp'];
             Util::getInstance()->saveActionWithLocalTime("Question Progress: Clicked Start",$qProgressID,$base,$localTime,$localDate,$localTimestamp);
-            
+
 			$base->setAllowBrowsing(0);
 			$base->setAllowBrowsing(1);
 			$_SESSION['refreshQuestionSidebar'] = 1; //Experimental variable
             header("Location: about:blank");
-			
+
 		}
         //		else if (isset($_POST['posttask']))
         else if (isset($_GET['answer']))
 		{
-			
+
             $query = 'SELECT value FROM actions WHERE projectID='.$projectID.' AND action="Question Progress: Start" ORDER BY timestamp DESC LIMIT 1';
             $connection = Connection::getInstance();
             $results = $connection->commit($query);
             $line = mysql_fetch_array($results, MYSQL_ASSOC);
-            
+
             $qProgressID = $line['value'];
             $answer_txt='';
-            
-            
-            
-            
+
+
+
+
             //			if (!isset($_POST['answer_hid']))
             //			{
             $query = "UPDATE questions_progress
@@ -101,15 +101,15 @@
             answer='$answer_txt',
             responses = responses+1
             WHERE qProgressID='$qProgressID'";
-            
+
             $connection = Connection::getInstance();
             $results = $connection->commit($query);
-            
+
             //Save action
             Util::getInstance()->saveAction("Question Progress: Answer",$qProgressID,$base);
             //				Util::getInstance()->saveActionWithLocalTime("Question Progress: Answer",$qProgressID,$base,$localTime,$localDate,$localTimestamp);
             //			}
-			
+
 			//Next stage
 			Util::getInstance()->moveToNextStage();
 		}
@@ -119,41 +119,41 @@
             $query = "SELECT min(timestamp) min_timestamp
             FROM session_progress
             WHERE stageID = '".$base->getStageID()."' and projectID = '".$base->getProjectID()."'";
-            
-            
+
+
             //Util::getInstance()->saveAction('min time stamp query',0,$base);
-            
-            
-            
+
+
+
             $connection = Connection::getInstance();
             $results = $connection->commit($query);
             $line = mysql_fetch_array($results, MYSQL_ASSOC);
             $limit = $base->getMaxTime();
-            
+
             if ($line['min_timestamp']<>'')
             {
                 //Util::getInstance()->saveAction('min time stamp query inside IF',0,$base);
-                
+
                 $base->setTaskStartTimestamp($line['min_timestamp']);
             }
-            
+
             if ($base->isTaskActive())
             {
                 $question = "";
                 $questionID = "";
                 $answer = "";
                 $altAnswer = "";
-                
+
                 //SELECT QUESTION IF THERE IS ONE OPEN
                 $query = "SELECT qProgressID, questionID, startTimestamp
                 FROM questions_progress
                 WHERE stageID = '".$base->getStageID()."' and projectID = '".$base->getProjectID()."' and (endTimestamp IS NULL OR responses<2) and skip<>$collaborativeStudy";
-                
+
                 $connection = Connection::getInstance();
                 $results = $connection->commit($query);
                 $numRows = mysql_num_rows($results);
                 $qProgressID = 0;
-                
+
                 //IF QUESTION OPEN EXIST
                 if ($numRows>0)
                 {
@@ -161,20 +161,20 @@
                     $qProgressID = $line['qProgressID'];
                     $questionID = $line['questionID'];
                     $questionStartingTimestamp = $line['startTimestamp'];
-                    
+
                     //Retrieve question
                     $qQuery = "SELECT question, answer, altAnswer
                     FROM questions_study
                     WHERE questionID = '$questionID'
                     AND topicAreaID = $topicAreaID"; //Added topic area ID
-                    
+
                     $connection = Connection::getInstance();
                     $results = $connection->commit($qQuery);
                     $line = mysql_fetch_array($results, MYSQL_ASSOC);
                     $question = $line['question'];
                     $answer = $line['answer'];
                     $altAnswer = $line['altAnswer'];
-                    
+
 //                    if($topicAreaID==0){
 //                        $query = "SELECT warmupItemID1,warmupItemID2 FROM users WHERE userID='$userID'";
 //                        $results = $connection->commit($query);
@@ -183,25 +183,25 @@
 //                        $id2 = $line['warmupItemID2'];
 //                        $xstring='';
 //                        $ystring='';
-//                        
+//
 //                        $query = "SELECT * FROM warmup_countries WHERE groupID='1' AND groupItemID='$id1'";
 //                        $results = $connection->commit($query);
 //                        $line = mysql_fetch_array($results, MYSQL_ASSOC);
 //                        $xstring=$line['countryName'];
-//                        
+//
 //                        $query = "SELECT * FROM warmup_countries WHERE groupID='2' AND groupItemID='$id2'";
 //                        $results = $connection->commit($query);
 //                        $line = mysql_fetch_array($results, MYSQL_ASSOC);
 //                        $ystring=$line['countryName'];
-//                        
+//
 //                        $question = str_replace("[XXX]",$xstring,$question);
 //                        $question = str_replace("[YYY]",$ystring,$question);
 //                    }
-                    
+
                     $base->setQuestionID($questionID);
                     $base->setQuestion($question);
                     $base->setQuestionStartTimestamp($questionStartingTimestamp);
-                    
+
                     Util::getInstance()->saveAction('Question Progress: Revisit',$qProgressID,$base);
                 }
                 else //IF PREVIOUS QUESTION WAS RESPONDED OR NO QUESTIONS HAS BEEN RESPONDED
@@ -209,7 +209,7 @@
                     //echo "NEW QUESTION<br />";
                     //Retrieve new question
                     //Added topic area ID
-                    
+
                     $qQuery = "SELECT questionID, question, answer, altAnswer
                     FROM questions_study
                     WHERE stageID='".$base->getStageID()."'
@@ -218,28 +218,28 @@
                     AND NOT `order` is NULL
                     ORDER BY `order` ASC
                     LIMIT 1";
-                    
+
                     $connection = Connection::getInstance();
                     $results = $connection->commit($qQuery);
                     $line = mysql_fetch_array($results, MYSQL_ASSOC);
                     $numRows = mysql_num_rows($results);
-                    
+
                     if ($numRows>0)
                     {
-                        
+
                         $questionID = $line['questionID'];
                         $question = $line['question'];
                         $answer = $line['answer'];
                         $altAnswer = $line['altAnswer'];
-                        
+
                         $qQuery = "INSERT INTO questions_progress (userID, projectID, stageID, questionID, startDate, startTime, startTimestamp)
                         VALUES ('".$base->getUserID()."','".$base->getProjectID()."','".$base->getStageID()."','$questionID','".$base->getDate()."','".$base->getTime()."','".$base->getTimestamp()."')";
-                        
+
                         $connection = Connection::getInstance();
                         $results = $connection->commit($qQuery);
                         $qProgressID = $connection->getLastID();
-//                        
-//                        
+//
+//
 //                        if($topicAreaID==0){
 //                            $query = "SELECT warmupItemID1,warmupItemID2 FROM users WHERE userID='$userID'";
 //                            $results = $connection->commit($query);
@@ -248,27 +248,27 @@
 //                            $id2 = $line['warmupItemID2'];
 //                            $xstring='';
 //                            $ystring='';
-//                            
+//
 //                            $query = "SELECT * FROM warmup_countries WHERE groupID='1' AND groupItemID='$id1'";
 //                            $results = $connection->commit($query);
 //                            $line = mysql_fetch_array($results, MYSQL_ASSOC);
 //                            $xstring=$line['countryName'];
-//                            
+//
 //                            $query = "SELECT * FROM warmup_countries WHERE groupID='2' AND groupItemID='$id2'";
 //                            $results = $connection->commit($query);
 //                            $line = mysql_fetch_array($results, MYSQL_ASSOC);
 //                            $ystring=$line['countryName'];
-//                            
+//
 //                            $question = str_replace("[XXX]",$xstring,$question);
 //                            $question = str_replace("[YYY]",$ystring,$question);
 //                        }
-                        
-                        
-                        
+
+
+
                         $base->setQuestionID($questionID);
                         $base->setQuestion($question);
                         $base->setQuestionStartTimestamp($base->getTimestamp());
-                        
+
                         Util::getInstance()->saveAction('Question Progress: Start',$qProgressID,$base);
                     }
                     else
@@ -276,83 +276,80 @@
                         Util::getInstance()->moveToNextStage();
                     }
                 }
-                
-                
-                
+
+
+
                 $base->setAllowBrowsing(1);
                 //Experimental code: else statement
     ?>
-<html>
-<head>
-<title>Search Task
-</title>
-<link rel="stylesheet" href="../study_styles/pure-release-0.5.0/buttons.css">
-<link rel="stylesheet" href="../study_styles/pure-release-0.5.0/forms.css">
-<link rel="stylesheet" href="../study_styles/custom/text.css">
-<link rel="stylesheet" href="../study_styles/custom/background.css">
 
-</head>
-<script type="text/javascript" src="js/util.js"></script>
-<script type="text/javascript"></script>
-<body class="body">
-<center>
-<br/>
-<table width="90%">
-<tr><th><h2>Task</h2></th></tr>
-<tr align="center"><td>
-</center>
-<p>
-<?php
-    //echo $isQuestionInTime."-".$isTaskInTime."-".$isPretaskQuestionnaireComplete."<br />";
-    
-    if (1)
-    {
-        
-        echo "The research topic you entered is:";
-        
-    }
-    
-    $base = Base::getInstance();
-    $connection = Connection::getInstance();
-    $userID = $base->getUserID();
-    $userID = $base->getProjectID();
-    $query = "SELECT * FROM recruits WHERE projectID='$projectID' ORDER BY recruitsID ASC";
-    $results = $connection->commit($query);
-    $question1 = '';
-    $question2 = '';
-    
-    $line = mysql_fetch_array($results,MYSQL_ASSOC);
-    $question1 = "IT Market Sector Analysis: The ITMSA project will involve an ongoing, information-driven investigation of ONE IT market sector (e.g., search engines, 3-D printers, tablets, smart phones, e-readers, social media, internet service providers, cloud computing, game development, etc.) aimed at discovering information about the sector, as well as TWO companies in that market sector that could be used for a business analysis (e.g., to advise a potential investor or other stakeholder as to the prospects for the sector and/or companies in the future). Group members will investigate the history of the sector and the companies, track news about the companies, follow the companies’ stock values, and look for independent analysis of the companies’ future based on published reports.";
-    
-    if($num_users == 2){
-        $line = mysql_fetch_array($results,MYSQL_ASSOC);
-        $question2 = "IT Market Sector Analysis: The ITMSA project will involve an ongoing, information-driven investigation of ONE IT market sector (e.g., search engines, 3-D printers, tablets, smart phones, e-readers, social media, internet service providers, cloud computing, game development, etc.) aimed at discovering information about the sector, as well as TWO companies in that market sector that could be used for a business analysis (e.g., to advise a potential investor or other stakeholder as to the prospects for the sector and/or companies in the future). Group members will investigate the history of the sector and the companies, track news about the companies, follow the companies’ stock values, and look for independent analysis of the companies’ future based on published reports.";
-    }
-    ?>
+		<html>
+		<head>
+		<title>Search Task
+		</title>
+		<link rel="stylesheet" href="../study_styles/pure-release-0.5.0/buttons.css">
+		<link rel="stylesheet" href="../study_styles/pure-release-0.5.0/forms.css">
+		<link rel="stylesheet" href="../study_styles/custom/text.css">
+		<link rel="stylesheet" href="../study_styles/custom/background.css">
 
-	
-</p>
-</td></tr>
-<tr><td><hr/></td></tr>
-<tr>
-<td>
+		</head>
+		<script type="text/javascript" src="js/util.js"></script>
+		<script type="text/javascript"></script>
+		<body class="body">
+		<center>
+		<br/>
+		<table width="90%">
+		<tr><th><h2>Task</h2></th></tr>
+		<tr align="center"><td>
+		</center>
+		<p>
+		<?php
+				//echo $isQuestionInTime."-".$isTaskInTime."-".$isPretaskQuestionnaireComplete."<br />";
 
-<br />
-<div class="grayrect">
-<span>
-<?php
-//    if($num_users == 2){
-//        echo "<strong><u>Topic 1</u></strong>";
-//        echo "<br><br>";
-//    }
-    echo $question1;
-//    if($num_users == 2){
-//        echo "<br><br>";
-//        echo "<strong><u>Topic 2</u></strong>";
-//        echo "<br><br>";
-//        echo $question2;
-//    }
-    ?>
+				if (1)
+				{
+
+						echo "The research topic you entered is:";
+
+				}
+
+				$base = Base::getInstance();
+						$connection = Connection::getInstance();
+						$userID = $base->getUserID();
+						$userID = $base->getProjectID();
+						$query = "SELECT Q.question as question FROM recruits R,questions_study Q WHERE R.projectID='$projectID' AND R.userID='$userID' AND R.instructorID+1=Q.questionID ORDER BY recruitsID ASC";
+						$results = $connection->commit($query);
+						$question1 = '';
+
+						$line = mysql_fetch_array($results,MYSQL_ASSOC);
+						$question1 = $line['question'];
+
+				?>
+
+
+		</p>
+		</td></tr>
+		<tr><td><hr/></td></tr>
+		<tr>
+		<td>
+
+		<br />
+		<div class="grayrect">
+		<span>
+			<?php
+			//    if($num_users == 2){
+			//        echo "<strong><u>Topic 1</u></strong>";
+			//        echo "<br><br>";
+			//    }
+					echo $question1;
+			//    if($num_users == 2){
+			//        echo "<br><br>";
+			//        echo "<strong><u>Topic 2</u></strong>";
+			//        echo "<br><br>";
+			//        echo $question2;
+			//    }
+					?>
+
 </span>
 </div>
 <br />
@@ -367,9 +364,9 @@ echo "<li>With your partner, search for sources on this topic to use for your re
 echo "<li>Search for sources on this topic to use for your research paper.</li>";
 }
 ?>
-	<li> When conducting this task, use snippets to collect information from web pages, and take notes to help you write your paper. </li> 
-	<li> You have <strong> 40 minutes </strong> to complete the task.</li> 
-	<li> Clicking the “Search” button below will open a new tab.</li> 
+	<li> When conducting this task, use snippets to collect information from web pages, and take notes to help you write your paper. </li>
+	<li> You have <strong> 40 minutes </strong> to complete the task.</li>
+	<li> Clicking the “Search” button below will open a new tab.</li>
 </ul>
 
 </td>
@@ -387,7 +384,7 @@ echo "<li>Search for sources on this topic to use for your research paper.</li>"
 </html>
 <?php
 	}
-    
+
 	}
     }
     ?>
--- spring2015/services/viewMyStuff_real.php
+++ spring2015/services/viewMyStuff_real.php
@@ -2,48 +2,48 @@
 	session_start();
 	require_once('../core/Base.class.php');
 	require_once('../core/Connection.class.php');
-	require_once('../core/Util.class.php');	
+	require_once('../core/Util.class.php');
 
     $base = new Base();
-	
+
 	if ($base->isSessionActive())
 	{
 //			$localTime = $_GET['localTime'];
 //			$localDate = $_GET['localDate'];
 //			$localTimestamp = $_GET['localTimestamp'];
-        
+
         $userID = $base->getUserID();
         $query = "SELECT numUsers from users WHERE userID='$userID'";
         $connection = Connection::getInstance();
         $results = $connection->commit($query);
         $line = mysql_fetch_array($results,MYSQL_ASSOC);
         $num_users = $line['numUsers'];
-			
+
         	$stageID = $base->getStageID();
 			$userID=$base->getUserID();
             $projectID=$base->getProjectID();
 			$qIDQuery = "SELECT questionID
 						FROM questions_progress
 						WHERE projectID=$projectID AND stageID=$stageID AND responses=0";
-		
-					
+
+
 			$connection = Connection::getInstance();
 			$qIDresults = $connection->commit($qIDQuery);
 			$numRows = mysql_num_rows($qIDresults);
 			$qIDline = mysql_fetch_array($qIDresults, MYSQL_ASSOC);
 			$questionID = $qIDline['questionID'];
-			
-        
+
+
         	Util::getInstance()->saveAction("View My Task",$questionID, $base);
-//        	Util::getInstance()->saveActionWithLocalTime("View My Task",$questionID,$base,$localTime,$localDate,$localTimestamp);	
-        
-       	 	$query = "SELECT question 
-       	 				FROM questions_study 
+//        	Util::getInstance()->saveActionWithLocalTime("View My Task",$questionID,$base,$localTime,$localDate,$localTimestamp);
+
+       	 	$query = "SELECT question
+       	 				FROM questions_study
        	 				WHERE questionID = $questionID";
 
  			$results = $connection->commit($query);
 			$numRows = mysql_num_rows($results);
-			
+
 			if ($numRows>0)
 			{
         		$base = Base::getInstance();
@@ -54,16 +54,16 @@
                 $results = $connection->commit($query);
                 $question1 = '';
                 $question2 = '';
-                
+
                 $line = mysql_fetch_array($results,MYSQL_ASSOC);
                 $question1 = $line['researchtopic'];
-                
+
                 if($num_users == 2){
                     $line = mysql_fetch_array($results,MYSQL_ASSOC);
                     $question2 = $line['researchtopic'];
                 }
 
-				  	
+
 ?>
 
 <html>
@@ -93,12 +93,10 @@
 </body>
 </html>
 
-<?php				
-				
+<?php
+
 			}
-        
-        
-	}        
-?>      
-        
-  
+
+
+	}
+?>
