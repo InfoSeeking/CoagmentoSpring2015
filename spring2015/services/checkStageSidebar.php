5c524afb375ed4ee44ae44213225a254229dbb8e
mmitsui <mmitsui@scarletmail.rutgers.edu>
2015-02-02 15:57:22 -0500

    questions_progress update

--- spring2015/services/checkStageSidebar.php
+++ spring2015/services/checkStageSidebar.php
@@ -2,15 +2,15 @@
 	session_start();
 	require_once('../core/Base.class.php');
 	require_once('../core/Connection.class.php');
-			
+
 	$base = Base::getInstance();
-	
-	
+
+
 	/*----- Chat communication is not required since this is for single participant------*/
-	
+
 	$allowCommunication = $base->getAllowCommunication();
 	if ($allowCommunication==1)
-	{	
+	{
 	  //Experimental: VERY RISKY ECHO!  Don't know if this echo "5" will work...
 	  if(isset($_SESSION['refreshQuestionSidebar']) && ($_SESSION['refreshQuestionSidebar']) == 1){
 	  	$_SESSION['refreshQuestionSidebar'] = 0;
@@ -24,50 +24,54 @@
 //	  else if (!$base->isTaskInTime())
 //	  {
 //		echo "2";
-//	  }	
+//	  }
 	  else
 	  {
-	    $query = "select 1 from questions_progress
-				  where stageID = '".$base->getStageID()."'
-  				    and projectID = '".$base->getProjectID()."'
-  					and responses > 0
-  					and questionID = '".$base->getQuestionID()."'";
-					
-		$connection = Connection::getInstance();
-		$results = $connection->commit($query);
-		$numRows = mysql_num_rows($results);
-		
-	
-		if ($numRows>0) //Someone already responded
-		{
-			echo "2";
-		}
-		else
-		if ($base->getStudyID()==1)
-		{
 			if ($base->getAllowBrowsing()==1)
 				echo "1";
 			else
 				echo "0";
-		}
-		else
-		{
-			if ($base->getStageID()==170)
-			{
-				if ($_SESSION['syncQuestion']==0)
-					echo "0";
-				else
-					if ($base->getAllowBrowsing()==1)
-						echo "1";
-					else
-						echo "2";
-			}
-			else
-				if ($base->getAllowBrowsing()==1)
-					echo "1";
-				else
-					echo "2";
-		}
+	  //   $query = "select 1 from questions_progress
+		// 		  where stageID = '".$base->getStageID()."'
+  	// 			    and projectID = '".$base->getProjectID()."'
+  	// 				and responses > 0
+  	// 				and questionID = '".$base->getQuestionID()."'";
+		//
+		// $connection = Connection::getInstance();
+		// $results = $connection->commit($query);
+		// $numRows = mysql_num_rows($results);
+
+
+		// if ($numRows>0) //Someone already responded
+		// {
+		// 	echo "2";
+		// }
+		// else
+		// if ($base->getStudyID()==1)
+		// {
+		// 	if ($base->getAllowBrowsing()==1)
+		// 		echo "1";
+		// 	else
+		// 		echo "0";
+		// }
+		// else
+		// {
+		// 	if ($base->getStageID()==170)
+		// 	{
+		// 		if ($_SESSION['syncQuestion']==0)
+		// 			echo "0";
+		// 		else
+		// 			if ($base->getAllowBrowsing()==1)
+		// 				echo "1";
+		// 			else
+		// 				echo "2";
+		// 	}
+		// 	else
+		// 		if ($base->getAllowBrowsing()==1)
+		// 			echo "1";
+		// 		else
+		// 			echo "2";
+		// }
 		//}
 		//else
 		//	echo "0";
@@ -76,46 +80,46 @@
 	else
 		echo "3";
 	//echo "2";
-	
-	if ($base->getStudyID()==1)
-	{
-		$query = "select 1 from questions_progress
-				  where stageID = '".$base->getStageID()."'
-  				    and projectID = '".$base->getProjectID()."'
-  					and responses = 1
-  					and questionID = '".$base->getQuestionID()."'";
-		
-		$connection = Connection::getInstance();
-		$results = $connection->commit($query);
-		$numRows = mysql_num_rows($results);
-		
-		
-		
-		if($numRows==1)
-		{
-			//refresh side bar in sidebar.php
-		}
-		else if($numRows==0)
-		{
-			// show side bar (echo "1")
-		}
-		/*
-		if ((($base->getStageID()==50)||($base->getStageID()==70)||($base->getStageID()==150)||($base->getStageID()==170))&&($base->getStudyID()>1))
-			echo "3";
-		else
-		if (($numRows>0)&&($base->getStageID()==100))
-		{
-			echo "2";
-		}
-		else
-			if ($base->getAllowBrowsing()==1)
-				echo "1";
-			else
-				echo "0";
-		*/
-	}
-	
-	
+
+	// if ($base->getStudyID()==1)
+	// {
+	// 	$query = "select 1 from questions_progress
+	// 			  where stageID = '".$base->getStageID()."'
+  // 				    and projectID = '".$base->getProjectID()."'
+  // 					and responses = 1
+  // 					and questionID = '".$base->getQuestionID()."'";
+	//
+	// 	$connection = Connection::getInstance();
+	// 	$results = $connection->commit($query);
+	// 	$numRows = mysql_num_rows($results);
+	//
+	//
+	//
+	// 	if($numRows==1)
+	// 	{
+	// 		//refresh side bar in sidebar.php
+	// 	}
+	// 	else if($numRows==0)
+	// 	{
+	// 		// show side bar (echo "1")
+	// 	}
+	// 	/*
+	// 	if ((($base->getStageID()==50)||($base->getStageID()==70)||($base->getStageID()==150)||($base->getStageID()==170))&&($base->getStudyID()>1))
+	// 		echo "3";
+	// 	else
+	// 	if (($numRows>0)&&($base->getStageID()==100))
+	// 	{
+	// 		echo "2";
+	// 	}
+	// 	else
+	// 		if ($base->getAllowBrowsing()==1)
+	// 			echo "1";
+	// 		else
+	// 			echo "0";
+	// 	*/
+	// }
+
+
 	/*
 	else
 	{
