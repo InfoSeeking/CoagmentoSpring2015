bec48b67cccee58bbf3ff77f15a88f077d8c8a18
mmitsui <mmitsui@scarletmail.rutgers.edu>
2015-02-27 16:29:19 -0500

    Comment out extra echo.

--- spring2015/core/Bookmark.class.php
+++ spring2015/core/Bookmark.class.php
@@ -70,7 +70,7 @@ class Bookmark extends Base {
   public static function update($bookmarkID, $notes="", $tags=array()){
     $cxn = Connection::getInstance();
     $query = sprintf("UPDATE bookmarks SET note='%s' WHERE bookmarkID=%d", $cxn->esc($notes), $bookmarkID);
-    echo $query;
+    // echo $query;
     $cxn->commit($query);
     $t = new Tags();
     $t->deleteForBookmark($bookmarkID);
