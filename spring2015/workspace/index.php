3c8b1105f70a46f199027531bc4c470806de7e91
mmitsui <mmitsui@scarletmail.rutgers.edu>
2015-03-02 22:08:00 -0500

    Questionnaire table

--- spring2015/workspace/index.php
+++ spring2015/workspace/index.php
@@ -8,6 +8,8 @@ require_once("../core/Query.class.php");
 require_once("../core/Tags.class.php");
 require_once("assets/php/util.php");
 require_once("../core/Connection.class.php");
+require_once("../core/Questionnaires.class.php");
+
 
 $base = new Base();
 if(!$base->isUserActive()){
@@ -23,6 +25,39 @@ if(isset($_GET["page"])){
 if(!in_array($PAGE, $valid_pages)){
   exit("Invalid page " . $PAGE);
 }
+
+$questionnaires = array();
+$a = array(array("first",1),array("second",2),array("third",3));
+$cxn = Connection::getInstance();
+$questionnaire = Questionnaires::getInstance();
+$userID = $base->getUserID();
+$projectID = $base->getProjectID();
+foreach($a as $v){
+  $word = $v[0];
+  $num = $v[1];
+  $r = $cxn->commit("SELECT I.questionnaire".(string)$num."start as questionnaire".(string)$num."start,I.questionnaire".(string)$num."end as questionnaire".(string)$num."end FROM recruits R,instructors I WHERE R.userID='$userID' AND R.instructorID=I.instructorID");
+  $line = mysql_fetch_array($r,MYSQL_ASSOC);
+  $startdatestr = date("M d",strtotime($line["questionnaire".(string)$num."start"]));
+  $enddatestr = date("M d",strtotime($line["questionnaire".(string)$num."end"]));
+  if($word =='third' && ($questionnaire->isQuestionnaireComplete("spring2015-midtask-third",array("$userID","$projectID"),array('userID','projectID'),'questionnaire_midtask_third')
+  && $questionnaire->isQuestionnaireComplete("spring2015-midtask-third-parttwo",array("$userID","$projectID"),array('userID','projectID'),'questionnaire_midtask_third_parttwo'))){
+    $questionnaires[$num] = array("complete",$startdatestr,$enddatestr,$word);
+  }else if($word != 'third'  &&
+  ($questionnaire->isQuestionnaireComplete("spring2015-midtask-$word",array("$userID","$projectID"),array('userID','projectID'),"questionnaire_midtask_$word"))){
+    $questionnaires[$num] = array("complete",$startdatestr,$enddatestr,$word);
+  }else if(strtotime($line["questionnaire".(string)$num."start"]) - time() >=0){
+    $questionnaires[$num] = array("early",$startdatestr,$enddatestr,$word);
+  }else if(time() - strtotime($line["questionnaire".(string)$num."end"]) >= 0){
+    $questionnaires[$num] = array("late",$startdatestr,$enddatestr,$word);
+  }else if(time() - strtotime($line["questionnaire".(string)$num."end"]) >= -259200){
+    $questionnaires[$num] = array("warning",$startdatestr,$enddatestr,$word);
+  }else{
+    $questionnaires[$num] = array("okay",$startdatestr,$enddatestr,$word);
+  }
+
+}
+
+
 
 $username = $base->getUserName();
 $userID = $base->getUserID();
