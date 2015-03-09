b829181190a7636b68074f7606aa4b46e9cd284e
mmitsui <mmitsui@scarletmail.rutgers.edu>
2015-03-09 19:45:08 -0400

    Cleaning up some dev files...

--- dev/CSpace/fcomments.php
+++
@@ -1,8 +0,0 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
-<head></head>
-<body>
-	comments<br/>
-	<script src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php" type="text/javascript"></script> <script type="text/javascript">  FB.init("c557d4912dee4ba94a7fc135502c0404", "xd_receiver.htm"); </script> 
-	<fb:comments></fb:comments> 
-</body>
-</html>
--- dev/CSpace/testing.php
+++
@@ -1,4 +0,0 @@
-<?php
-$homepage = file_get_contents('http://www.msn.com/');
-echo "<table width=50%><tr><td>$homepage</td></tr></table>";
-?>
