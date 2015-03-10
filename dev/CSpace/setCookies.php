808e851f043bac993b9ff5ca18bcac78e8696fe2
mmitsui <mmitsui@scarletmail.rutgers.edu>
2015-03-09 22:24:36 -0400

    Last changes for now

--- dev/CSpace/setCookies.php
+++
@@ -1,7 +0,0 @@
-<?php
-	setcookie("CSpace_login", '3');
-	setcookie("CSpace_projectID", '1');
-	require_once('header.php');	
-	echo "<br/>Cookies set.<br/><br/><br/>\n";
-	require_once('footer.php');
-?>
