13b0ede69df3e24dc6eadf95cbb7fedcb754e9a0
mmitsui <mmitsui@scarletmail.rutgers.edu>
2015-06-22 13:55:51 -0400

    Things.

---
+++ summer2015/signup_intro.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Study Registration Closed</title>
+<link rel="stylesheet" href="study_styles/pure-release-0.5.0/buttons.css">
+<link rel="stylesheet" href="study_styles/pure-release-0.5.0/forms.css">
+<link rel="stylesheet" href="study_styles/custom/text.css">
+
+
+</head>
+<body class="style1">
+
+Registration for the Summer 2015 study has reached capacity and has been officially closed. We apologize for the inconvenience but would like to thank you for your interest in participating.
+
+
+</body>
+</html>
